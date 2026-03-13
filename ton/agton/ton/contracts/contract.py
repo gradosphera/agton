3463f1e83bf4dc2d9453a863fdc1e225ae63aa34
Alexey Gosunov <agosunov@gmail.com>
2026-03-13 10:11:31 +0300

    feat(ton): add useful methods to `Contract` base class

--- ton/agton/ton/contracts/contract.py
+++ ton/agton/ton/contracts/contract.py
@@ -1,32 +1,30 @@
 from __future__ import annotations
 
-from typing import Self, Iterable
+from typing import Self, Iterable, Iterator
 
 from ..cell.cell import Cell
 
 from ..provider import Provider
-from ..types import Address, StateInit, MessageRelaxed, Message, CurrencyCollection
+from ..types import Address, StateInit, MessageRelaxed, Message, CurrencyCollection, ExtraCurrencyCollection
 from ..types import MsgAddressExt, AddrNone
+from ..types import Transaction
+from ..types import Account, AccountOrdinary, ActiveAccountState, UninitAccountState, FrozenAccountState
 from ..types.tvm_value import TvmValue
 
 class ContractError(Exception):
     pass
 
 class Contract:
-    def __init__(self, address: Address, provider: Provider | None = None) -> None:
+    def __init__(self, address: Address, provider: Provider) -> None:
         self.address = address
         self.provider = provider
     
     def run_get_method(self,
                        method_id: int | str,
-                       stack: Iterable[TvmValue] | TvmValue | None = None) -> tuple[TvmValue, ...] | TvmValue:
-        if self.provider is None:
-            raise ContractError('Cannot run get method without provider set')
+                       stack: Iterable[TvmValue] | TvmValue = ()) -> tuple[TvmValue, ...] | TvmValue:
         return self.provider.run_get_method(self.address, method_id, stack)
     
     def send_external_message(self, msg: Message) -> bytes:
-        if self.provider is None:
-            raise ContractError('Cannot send external message without provider set')
         return self.provider.send_external_message(msg)
     
     def create_internal_message(self, *,
@@ -52,3 +50,42 @@ class Contract:
             body=body,
             init=init
         )
+
+    def get_account_state(self) -> Account:
+        return self.provider.get_account_state(self.address)
+    
+    def get_transactions(self) -> Iterator[Transaction]:
+        return self.provider.get_account_transactions(self.address)
+
+    def get_balance_with_extracurrency(self) -> CurrencyCollection:
+        account = self.get_account_state()
+        if not isinstance(account, AccountOrdinary):
+            return CurrencyCollection(0, ExtraCurrencyCollection())
+        return account.storage.balance
+    
+    def get_balance(self) -> int:
+        return self.get_balance_with_extracurrency().grams
+    
+    def get_state_init(self) -> StateInit | None:
+        account = self.get_account_state()
+        if not isinstance(account, AccountOrdinary):
+            return None
+        state = account.storage.state
+        if not isinstance(state, ActiveAccountState):
+            return None
+        return state.state_init
+    
+    def is_deployed(self) -> bool:
+        return self.get_state_init() is not None
+
+    def get_code(self) -> Cell | None:
+        s = self.get_state_init()
+        if s is None:
+            return None
+        return s.code
+    
+    def get_data(self) -> Cell | None:
+        s = self.get_state_init()
+        if s is None:
+            return None
+        return s.data
